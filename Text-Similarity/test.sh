278468674d5c1a4e05a97cebb7f85c018aa580b4
Seasa_Huang <ericet1234@gmail.com>
2019-05-06 18:30:52 +0800

    add up testing script

---
+++ Text-Similarity/test.sh
@@ -0,0 +1,14 @@
+model='qalstm'
+#word=('all_no' 'part_no')
+word=('part_no')
+pred=('two_class' 'two_regression' 'three_class')
+
+
+for((i=0;$i<${#word[@]};i=i+1))
+do
+    for((j=0;$j<${#pred[@]};j=j+1))
+    do
+		echo ${model}_${word[i]}_${pred[j]}_ban
+        python test.py --save ${model}_${word[i]}_${pred[j]} --data ./data/${word[i]}_embedding/test.csv --out ./saved_models/${model}_${word[i]}_${pred[j]}/pred
+    done
+done
